1f8cc7f65a7bfd6c68dea73a75a99924b2648f70
AxeemHaider <axeemhaider@gmail.com>
2020-02-15 11:25:57 +0500

    Added test for image usecase

---
+++ tests/v100/test_usecase_image.py
@@ -0,0 +1,21 @@
+from quran.domain.image import Image
+from tests.quran.factory.image_factory import ImageFactory
+
+
+def test_create_image():
+    data = {
+        'ayah_id': 'ayah-id-4',
+        'image': 'image url for ayah 4'
+    }
+    image = Image.from_dict(data)
+    create_image = ImageFactory.create()
+    image = create_image.exec(image)
+
+    assert image.to_dict() == data
+
+
+def test_find_image_by_ayah_id():
+    find_image = ImageFactory.find_image()
+    image = find_image.by_ayah_id('ayah-id-3')
+
+    assert image.ayah_id == 'ayah-id-3'

bb9a6874b9d2bf0a106370a862ffc6e7c1bd76ec
AxeemHaider <axeemhaider@gmail.com>
2020-02-28 19:33:04 +0500

    Correcting audio proto

---
+++ quran/endpoints/Services/AudioService.py
@@ -0,0 +1,28 @@
+import quran.endpoints.grpc.audio_pb2 as audio_proto
+import quran.endpoints.grpc.audio_pb2_grpc as audio_rpc
+import quran.endpoints.grpc.entity_pb2 as entity_proto
+from quran.domain.audio import Audio
+from quran.factory.audio_factory import AudioFactory
+from quran.utils.proto_converter import ProtoConverter
+
+
+class AudioService(audio_rpc.AudioServicer):
+
+    def CreateAudio(self, request, context):
+        audio = Audio.from_dict(ProtoConverter.proto_to_dict(request))
+        create_audio = AudioFactory.create_audio()
+        res = create_audio.exec(audio)
+        return entity_proto.AudioEntity(**res.to_dict())
+
+    def FindAudioById(self, request, context):
+        find_audio = AudioFactory.find_audio()
+        audio = find_audio.by_id(request.id)
+        return entity_proto.AudioEntity(**audio.to_dict())
+
+    def FindAudioByAyahId(self, request, context):
+        find_audio = AudioFactory.find_audio()
+        audio = find_audio.by_ayah_id(request.id)
+        return entity_proto.AudioEntity(**audio.to_dict())
+
+    def FindAudioByEditionId(self, request, context):
+        pass
--- quran/endpoints/Services/ImageService.py
+++ quran/endpoints/Services/ImageService.py
@@ -1,5 +1,6 @@
 import quran.endpoints.grpc.image_pb2_grpc as image_rpc
 import quran.endpoints.grpc.entity_pb2 as entity_proto
+from quran.utils.proto_converter import ProtoConverter
 from quran.domain.image import Image
 from quran.factory.image_factory import ImageFactory
 
@@ -7,7 +8,12 @@ from quran.factory.image_factory import ImageFactory
 class ImageService(image_rpc.ImageServicer):
 
     def CreateImage(self, request, context):
-        image = Image.from_dict(request)
+        image = Image.from_dict(ProtoConverter.proto_to_dict(request))
         create_image = ImageFactory.create()
         res = create_image.exec(image)
         return entity_proto.ImageEntity(**res.to_dict())
+
+    def FindImageByAyahId(self, request, context):
+        find_image = ImageFactory.find_image()
+        image = find_image.by_ayah_id(request.id)
+        return entity_proto.ImageEntity(**image.to_dict())

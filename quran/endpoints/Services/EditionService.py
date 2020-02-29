3edec046e992808ac154b0d89b2f5a4b1ec593c9
AxeemHaider <axeemhaider@gmail.com>
2020-02-29 10:39:15 +0500

    edition service complete

--- quran/endpoints/Services/EditionService.py
+++ quran/endpoints/Services/EditionService.py
@@ -14,3 +14,42 @@ class EditionService(edition_rpc.EditionServicer):
         res = create_edition.exec(edition)
 
         return entity_proto.EditionEntity(**res.to_dict())
+
+    def GetAll(self, request, context):
+        find_edition = EditionFactory.find_edition()
+        edition_stream = find_edition.get_all()
+        editions = []
+        for edition in edition_stream:
+            editions.append(entity_proto.EditionEntity(**edition.to_dict()))
+
+        return edition_proto.EditionList(edition_entity=editions)
+
+    def FindById(self, request, context):
+        find_edition = EditionFactory.find_edition()
+        edition = find_edition.by_id(request.id)
+        return entity_proto.EditionEntity(**edition.to_dict())
+
+    def FindByLanguage(self, request, context):
+        find_edition = EditionFactory.find_edition()
+        edition = find_edition.by_language(request.name)
+        return entity_proto.EditionEntity(**edition.to_dict())
+
+    def FindByName(self, request, context):
+        find_edition = EditionFactory.find_edition()
+        edition = find_edition.by_name(request.name)
+        return entity_proto.EditionEntity(**edition.to_dict())
+
+    def FindByEnglishName(self, request, context):
+        find_edition = EditionFactory.find_edition()
+        edition = find_edition.by_english_name(request.name)
+        return entity_proto.EditionEntity(**edition.to_dict())
+
+    def FindByFormat(self, request, context):
+        find_edition = EditionFactory.find_edition()
+        edition = find_edition.by_format(request.name)
+        return entity_proto.EditionEntity(**edition.to_dict())
+
+    def FindByType(self, request, context):
+        find_edition = EditionFactory.find_edition()
+        edition = find_edition.by_type(request.name)
+        return entity_proto.EditionEntity(**edition.to_dict())

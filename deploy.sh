75c4edb255f7b6ef7d10b3acc3e4a5fc4c6db17c
AxeemHaider <axeemhaider@gmail.com>
2020-03-29 16:09:36 +0500

    update travis

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-gcloud builds submit --tag gcr.io/utopian-button-227405/python-grpc-quran:$SHA .
+gcloud builds submit --tag gcr.io/$PROJECT_ID/python-grpc-quran:$SHA .
 
 kubectl apply -f ./deployment.yaml
-kubectl set image deployments/quran-grpc python-grpc-quran=gcr.io/utopian-button-227405/python-grpc-quran:$SHA
+kubectl set image deployments/quran-grpc python-grpc-quran=gcr.io/$PROJECT_ID/python-grpc-quran:$SHA
